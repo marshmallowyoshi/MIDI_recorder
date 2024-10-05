7a40590aa62e75dbc8ced4e5b020528db84703a6
marshmallowyoshi <97353790+marshmallowyoshi@users.noreply.github.com>
2024-10-05 20:55:20 +0100

    add requirements

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name='midi_rec_service',
     version='0.0.1',
     packages=['midi_rec_service', 'midi_rec_service.CK_rec'],
+    install_requires=[
+        'python-rtmidi',
+        'mido'
+    ],
     entry_points={
         'console_scripts': [
             'midirec = midi_rec_service.midisrv:main'
